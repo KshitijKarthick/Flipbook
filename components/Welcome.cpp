66579e8f483d446d1acc986bab99cf9fec316221
Kshitij Karthick <kshitij.karthick@gmail.com>
2015-05-18 09:11:51 +0530

    Lighting and shading fixed, with text placements based on image

--- components/Welcome.cpp
+++ components/Welcome.cpp
@@ -27,11 +27,11 @@ void renderText(char *string){
 */
 void showHeading(){
     char heading[] = "FlipBook";
-    GLfloat LIGHT_AMBIENT[]={0.5f,0.59f, 0.57f, 0.0f};
-    GLfloat LIGHT_DIFFUSE[]={0.73f,0.74f,0.76f,0.22f};
-    GLfloat LIGHT_POSITION[]={200.0f, 500.0f, 0.0f, 1.0f};
-    GLfloat mat_ambient[]={0.7f,0.7f,0.7f,1.0f};
-    GLfloat mat_diffuse[]={0.5f,0.5f,0.5f,1.0f};
+    GLfloat LIGHT_AMBIENT[]={0.04f,0.09f, 0.07f, 1};
+    GLfloat LIGHT_DIFFUSE[]={0.73f,0.74f,0.46f,1};
+    GLfloat LIGHT_POSITION[]={550.0f, 500.0f, 100.0f, 1.0f};
+    GLfloat mat_ambient[]={0.3f,0.2f,0.4f,1.0f};
+    GLfloat mat_diffuse[]={0.3f,0.5f,0.7f,1.0f};
     GLfloat mat_specular[]={1.0f,1.0f,1.0f,1.0f};
     GLfloat mat_shininess[]={50.0f};
     /*!< Lighting and Shading */
@@ -39,21 +39,21 @@ void showHeading(){
     glEnable(GL_LIGHT0);
     glLightfv(GL_LIGHT0, GL_AMBIENT, LIGHT_AMBIENT);
     glLightfv(GL_LIGHT0, GL_DIFFUSE, LIGHT_DIFFUSE);
-    GLfloat LIGHT_AMBIENT1[]={0.7f,0.7f, 0.27f, 0.0f};
-    GLfloat LIGHT_DIFFUSE1[]={0.33f,0.54f,0.86f,0.22f};
+    GLfloat LIGHT_AMBIENT1[]={0.4f,0.4f, 0.47f, 1};
+    GLfloat LIGHT_DIFFUSE1[]={0.53f,0.74f,0.66f,1};
     glEnable(GL_LIGHT1);
     glLightfv(GL_LIGHT1, GL_AMBIENT, LIGHT_AMBIENT1);
     glLightfv(GL_LIGHT1, GL_DIFFUSE, LIGHT_DIFFUSE1);
     glLightfv(GL_LIGHT1, GL_POSITION, LIGHT_POSITION);
     glLineWidth(2);
-    glColor3f(1,1,1);
+    //glColor3f(0.3,0,0.5);
     glPushMatrix();
         glLoadIdentity();
         glMaterialfv(GL_FRONT,GL_AMBIENT,mat_ambient);
         glMaterialfv(GL_FRONT,GL_DIFFUSE,mat_diffuse);
         glMaterialfv(GL_FRONT,GL_SPECULAR,mat_specular);
         glMaterialfv(GL_FRONT,GL_SHININESS,mat_shininess);
-        glTranslatef(-300,250,0);
+        glTranslatef(-300,450,0);
         glScalef(1.5,1.5,10);
         glRotatef(pageAngle, 1 , 0 ,0);
         glBegin(GL_LINES);
@@ -67,6 +67,7 @@ void showHeading(){
     glDisable(GL_LIGHT1);
     glDisable(GL_LIGHTING);
     glLineWidth(1);
+    glColor3f(1,1,1);
 }
 
 /*!
@@ -75,13 +76,13 @@ void showHeading(){
     Credits for the project are displayed under this section using lighting and shading.
 */
 void showCredits(){
-    char developer[][50] = {"Kshitij Karthick - 1RE12CS042","Srinivas Ananth - 1RE12CS034"};
+    char developer[][50] = {"Kshitij Karthick 1RE12CS042","Srinivas Ananth 1RE12CS034"};
     GLfloat LIGHT_AMBIENT[] =   //< light ambience
-        {0.0f,0.999f, 0.57f, 0.0f};
+        {0.3f,0.3f, 0.57f, 1};
     GLfloat LIGHT_DIFFUSE[] =   //< light diffusion
-        {0.99f,0.99f,0.99f,0.99f};
+        {0.3f,0.5f,0.7,1};
     GLfloat LIGHT_POSITION[] =  //< light position
-        {-322.0f, -300.0f, 350.0f, 999.0f};
+        {-322.0f, -500.0f, 350.0f, 1};
     /*! Enable lighting and shading */
     glEnable(GL_LIGHT0);
     glEnable(GL_LIGHTING);
@@ -89,17 +90,16 @@ void showCredits(){
     glLightfv(GL_LIGHT0, GL_DIFFUSE, LIGHT_DIFFUSE);
     glLightfv(GL_LIGHT0, GL_POSITION, LIGHT_POSITION);
     glLineWidth(2);
-    glColor3f(1,1,1);
     glPushMatrix();
         glLoadIdentity();
-        glTranslatef(-150,-400,0);
+        glTranslatef(-880,-400,0);
         glScalef(0.5,0.5,1);
         renderText(developer[0]);
         glFlush();
     glPopMatrix();
     glPushMatrix();
         glLoadIdentity();
-        glTranslatef(-150,-500,0);
+        glTranslatef(-880,-500,0);
         glScalef(0.5,0.5,1);
         renderText(developer[1]);
         glFlush();
@@ -112,10 +112,10 @@ void showCredits(){
 /*!
     \fn showFooter()
     \brief Show footer message
-    Show footer message for instructions to be followed by the user as a marquee.    
+    Show footer message for instructions to be followed by the user as a marquee.
 */
 void showFooter(){
-    char message[]="Press Enter to Continue to the Flipbook";
+    char message[]="Press Enter to Continue to the Flipbook, +/- to Speed Control, n/p Page Control, W to go to welcome screen, Q for quit";
     glLineWidth(2.0);
     glPushMatrix();
         glLoadIdentity();
@@ -153,6 +153,7 @@ void runWelcomeScreen(){
         glTexCoord2f(0, 1); glVertex3f(-WINDOW_WIDTH, WINDOW_HEIGHT, -1000);
         glNormal3f(-WINDOW_WIDTH, WINDOW_HEIGHT, -1000);
     glEnd();
+    glDisable(GL_TEXTURE_2D);
     glPopMatrix();
     showHeading();
     showCredits();
